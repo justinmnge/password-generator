58a9b490eae25a5ac6661e57dd4909bd06ae46f4
justinmnge <justin.mnge@gmail.com>
2026-09-15 20:09:05 -0600

    Removed fontFamily in index.js

--- index.js
+++ index.js
@@ -122,7 +122,6 @@ function password() {
     generatePassword1 += availableCharacters[randomIndex1];
   }
   password1.textContent = generatePassword1;
-  ((password1.style.fontFamily = "Karla"), sans - serif);
 
   // PASSWORD 2
   let generatePassword2 = "";
@@ -132,7 +131,6 @@ function password() {
     generatePassword2 += availableCharacters[randomIndex2];
   }
   password2.textContent = generatePassword2;
-  ((password2.style.fontFamily = "Karla"), sans - serif);
 }
 
 function toggleSymbols() {
